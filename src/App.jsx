2b7f7fe6902522c7c4cf3411d2eebf7b863535ca
Quabmac <mccormack.jacob@gmail.com>
2023-12-22 22:37:55 +1100

    Updated fetch request to use api instead of manual

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,46 @@
 import { useState, useEffect } from "react"
+import Airtable from "airtable"
 import "./App.css"
 import Card from "./components/Card"
 
 function App() {
-  /* Initial variables */
   const [data, setData] = useState([])
   const [search, setSearch] = useState("")
-  const [tags, setTags] = useState([])
-  const [selectedTag, setSelectedTag] = useState([])
 
   useEffect(() => {
-    /* Fetch Variables */
-    const baseID = "appOOlZ2AISbHpbVb"
-    const tableName = "All Clips"
-    const AIRTABLE_API_KEY =
-      "patGrTqLMArSkLwSf.28b21052d73212484e726c2573b482facab396c5f5dc83919af2e484611cc6b4"
+    var base = new Airtable({
+      apiKey:
+        "patGrTqLMArSkLwSf.28b21052d73212484e726c2573b482facab396c5f5dc83919af2e484611cc6b4",
+    }).base("appOOlZ2AISbHpbVb")
 
-    /* Function to recursively fetch all records */
-    const fetchAllRecords = (offset, allRecords = []) => {
-      let fetchURL = `https://api.airtable.com/v0/${baseID}/${tableName}`
+    let allRecords = []
 
-      if (offset) {
-        fetchURL += `?offset=${offset}`
-      }
-
-      fetch(fetchURL, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-          "Content-Type": "application/json",
-        },
+    base("All Clips")
+      .select({
+        fields: ["Title", "URL", "ImageURL", "Rating", "Tags"],
+        sort: [{ field: "ID", direction: "desc" }],
       })
-        .then((response) => response.json())
-        .then((data) => {
-          const newRecords = allRecords.concat(data.records)
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
 
-          if (data.offset) {
-            // If there's an offset, more records are available, so fetch them
-            fetchAllRecords(data.offset, newRecords)
-          } else {
-            // No more records, process data
-            processData(newRecords)
+          records.forEach(function (record) {
+            allRecords.push(record.fields)
+          })
+
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage()
+        },
+        function done(err) {
+          if (err) {
+            console.error(err)
+            return
           }
-        })
-    }
-
-    /* Function to process data and sort tags */
-    const processData = (records) => {
-      console.log("Original data: ", records) // Log original data
-
-      // Sort records by ID
-      records.sort((a, b) => b.fields.ID - a.fields.ID)
-
-      const processedData = records.map((item) => ({
-        id: item.fields.ID,
-        created: item.fields["Created Date"],
-        title: item.fields.Title,
-        image: item.fields.ImageURL,
-        url: item.fields.URL,
-        tags: item.fields.Tags,
-        rating: item.fields.Rating,
-      }))
-
-      console.log("Processed data:", processedData) // Log processed data
-
-      // Extract and sort unique tags from processed data
-      const uniqueTags = Array.from(
-        new Set(processedData.flatMap((item) => item.tags || []))
-      ).sort()
-      setTags(uniqueTags)
-      console.log("Unique tags: ", uniqueTags)
-
-      // Set your state with processed data
-      setData(processedData)
-    }
-
-    // Initial call to start fetching records
-    fetchAllRecords()
+          setData(allRecords)
+        }
+      )
   }, [])
 
   const handleSearchChange = (event) => {
@@ -84,58 +48,37 @@ function App() {
   }
 
   const filteredData = data.filter((item) => {
-    // Check if item title matches the search
-    const matchesSearch = item.title
-      .toLowerCase()
-      .includes(search.toLowerCase())
+    // Convert search term to lowercase for case-insensitive comparison
+    const searchTerm = search.toLowerCase()
 
-    /// Check if all selected tags are present in the item's tags
-    const matchesTags =
-      selectedTag.length === 0 ||
-      selectedTag.every((tag) => item.tags.includes(tag))
+    // Check if the search term is in the 'Title' field
+    const titleMatch =
+      item.Title && item.Title.toLowerCase().includes(searchTerm)
 
-    return matchesSearch && matchesTags
+    // Check if the search term is in the 'Tags' field (assuming 'Tags' is an array of strings)
+    const tagsMatch =
+      item.Tags &&
+      item.Tags.some((tag) => tag.toLowerCase().includes(searchTerm))
+
+    // Return true if the item matches the search term in either 'Title' or 'Tags'
+    return titleMatch || tagsMatch
   })
-
-  const handleTagClick = (tag) => {
-    setSelectedTag((prevSelectedTags) => {
-      if (prevSelectedTags.includes(tag)) {
-        // If tag is already selected, remove it
-        return prevSelectedTags.filter((t) => t !== tag)
-      } else {
-        // If tag is not selected, add it
-        return [...prevSelectedTags, tag]
-      }
-    })
-  }
 
   return (
     <div className="App">
       <div className="Header-Container">
         <input
           type="text"
-          placeholder={`Displaying ${filteredData.length} records`}
+          placeholder={`(Version 2.0) ${filteredData.length} records`}
           value={search}
-          onChange={handleSearchChange}
+          onChange={handleSearchChange} // Use the handleSearchChange here
         />
       </div>
-      <div className="Grouped-Tags-Container">
-        {tags.map((tag) => (
-          <span
-            key={tag}
-            className={
-              selectedTag.includes(tag) ? "Highlighted-Tags" : "Grouped-Tags"
-            }
-            onClick={() => handleTagClick(tag)}
-          >
-            {tag}
-          </span>
-        ))}
-      </div>
+      <div className="Grouped-Tags-Container">{/* ... other code ... */}</div>
       <div className="Card-Container">
-        {filteredData.map((item) => {
-          return <Card key={item.id} data={item} />
-        })}
+        {filteredData.map((item, index) => (
+          <Card key={index} data={item} />
+        ))}
       </div>
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ import { IoCopy, IoCopyOutline } from "react-icons/io5"
 import PropTypes from "prop-types"
 
 const Card = ({ data }) => {
-  const { title, url, image, tags } = data
+  const { Title, URL, ImageURL, Tags } = data
   const [copyIcon, SetCopyIcon] = useState(<IoCopy />)
 
-  const copyToClipboard = async (title, url) => {
-    await navigator.clipboard.writeText(`${title}: ${url}`)
+  const copyToClipboard = async (title, URL) => {
+    await navigator.clipboard.writeText(`${title}: ${URL}`)
 
     SetCopyIcon(<IoCopyOutline />)
 
@@ -22,25 +22,25 @@ const Card = ({ data }) => {
       <div className="Ind-Image-And-Title-Container">
         <div
           className="Ind-Copy-Container"
-          onClick={() => copyToClipboard(title, url)}
+          onClick={() => copyToClipboard(Title, URL)}
         >
           <div className="Ind-Copy">{copyIcon}</div>
         </div>
         <div
           className="Ind-Open-Container"
-          onClick={() => window.open(url, "_blank")}
+          onClick={() => window.open(URL, "_blank")}
         >
           <IoOpen className="Ind-Open" />
         </div>
         <div className="Ind-Overlay">
-          <img src={image} />
+          <img src={ImageURL} />
         </div>
         <div className="Ind-Title-Container">
-          <h1 className="Ind-Title">{title}</h1>
+          <h1 className="Ind-Title">{Title}</h1>
         </div>
       </div>
       <div className="Ind-Tags-Container">
-        {tags.map((tag) => (
+        {Tags.map((tag) => (
           <span className="Ind-Tags" key={tag}>
             {tag}
           </span>
@@ -52,11 +52,11 @@ const Card = ({ data }) => {
 
 Card.propTypes = {
   data: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    URL: PropTypes.string.isRequired,
     created: PropTypes.string, // Assuming created is a string. Adjust type as necessary.
-    image: PropTypes.string, // Assuming image is a string URL.
-    tags: PropTypes.arrayOf(PropTypes.string), // Assuming tags is an array of strings.
+    ImageURL: PropTypes.string, // Assuming image is a string URL.
+    Tags: PropTypes.arrayOf(PropTypes.string), // Assuming tags is an array of strings.
     rating: PropTypes.number, // Adjust type if rating is not a number.
   }).isRequired,
 }
